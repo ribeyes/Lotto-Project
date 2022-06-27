17e6d6da81d8e32736b11f75d798f9ccca98d9c8
ircp <ircp@DESKTOP-COU6IAS>
2022-06-27 17:42:10 +0900

    6/27 까지 작업한거

--- Lotto/src/main/java/egovframework/example/controller/LottoController.java
+++ Lotto/src/main/java/egovframework/example/controller/LottoController.java
@@ -1,12 +1,15 @@
 package egovframework.example.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import egovframework.example.service.LottoService;
@@ -17,12 +20,28 @@ public class LottoController {
 	@Resource(name="lottoService")
 	private LottoService lottoService;
 	
+	@RequestMapping("/login.do")
+	public String loginPage() throws Exception {
+		return "login";
+	}
+	
+	@RequestMapping("/signUp.do")
+	public String signUpPage() throws Exception {
+		return "signUp";
+	}
+	
+	@RequestMapping(value="/post", method=RequestMethod.POST)
+	public void testPost(@RequestBody HashMap<String, Object> map) throws Exception { 
+		//List<Map> nameModel = lottoService.getName();
+		System.out.println(map);
+		
+	}
+	
 	@RequestMapping("/view.do")
 	public ModelAndView dbPage(ModelAndView mav) throws Exception { 
         
 		List<Map> nameModel = lottoService.getName();
 		
-		mav.addObject("name", nameModel); 
 		mav.setViewName("view");
 		
 		return mav;
---
+++ Lotto/src/main/java/egovframework/example/vo/User.java
@@ -0,0 +1,23 @@
+package egovframework.example.vo;
+
+public class User {
+	private String userId;
+
+	public User() {
+	}
+	
+	public User(String userId) {
+		super();
+		this.userId = userId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	
+}
